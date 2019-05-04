a67b060edb059f0f9d092e980d9cdc3367b7dd80
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2019-05-04 11:12:13 +0300

    Convert to FEF gh-24
    Backend Categories browse view

--- component/backend/ViewTemplates/Categories/default.blade.php
+++ component/backend/ViewTemplates/Categories/default.blade.php
@@ -61,5 +61,52 @@ defined('_JEXEC') or die();
 @stop
 
 @section('browse-table-body-withrecords')
-{{--  TODO  --}}
+	<?php $i = 0; ?>
+    @foreach($this->items as $row)
+        <tr>
+            <td>
+                @jhtml('FEFHelper.browse.order', 'ordering', $row->ordering)
+            </td>
+            <td>
+                @jhtml('FEFHelper.browse.id', ++$i, $row->getId())
+            </td>
+            <td>
+                <a href="@route(\FOF30\Utils\FEFHelper\BrowseView::parseFieldTags('index.php?option=com_docimport&view=Categories&task=edit&id=[ITEM:ID]', $row))">
+                    {{{ $row->title }}}
+                </a>
+            </td>
+            <td>
+                <span class="hasTooltip" title="@lang('COM_DOCIMPORT_CATEGORIES_STATUS_' . $row->status)">
+                    @if($row->status == 'missing')
+                        <span class="akeeba-label--red">
+                            <span class="akion-android-cancel"></span>
+                        </span>
+                    @elseif ($row->status == 'modified')
+                        <span class="akeeba-label--orange">
+                            <span class="akion-android-warning"></span>
+                        </span>
+                    @else
+                        <span class="akeeba-label--green">
+                            <span class="akion-checkmark"></span>
+                        </span>
+                    @endif
+                </span>
+                &nbsp;
+                <button
+                        onclick="window.location='index.php?option=com_docimport&view=Categories&task=rebuild&id={{ (int)$row->getId() }}';return false;"
+                        class="btn {{ ($row->status == 'modified') ? 'btn-primary btn-mini' : 'btn-inverse btn-mini' }}"
+                        title="<?php echo JText::_('COM_DOCIMPORT_CATEGORIES_REBUILD') ?>">
+                    <span class="icon-white icon-refresh"></span>
+                </button>
+            </td>
+            <td>
+                @jhtml('FEFHelper.browse.published', $row->enabled, $i)
+            </td>
+            <td>
+                {{{ \FOF30\Utils\FEFHelper\BrowseView::getOptionName($row->language, \FOF30\Utils\SelectOptions::getOptions('languages', ['none' => 'COM_DOCIMPORT_COMMON_FIELD_LANGUAGE_ALL'])) }}}
+            </td>            <td>
+                {{{ \FOF30\Utils\FEFHelper\BrowseView::getOptionName($row->access, \FOF30\Utils\SelectOptions::getOptions('access')) }}}
+            </td>
+        </tr>
+    @endforeach
 @stop
