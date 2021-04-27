fc2d29ed809edfcd4e3a8ed037f4dc197689e407
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2021-04-27 21:05:38 +0300

    Convert to core MVC
    Work around laughable issue in core code...

--- component/backend/src/Model/ArticleModel.php
+++ component/backend/src/Model/ArticleModel.php
@@ -10,11 +10,29 @@ namespace Akeeba\Component\DocImport\Administrator\Model;
 defined('_JEXEC') || die;
 
 use Joomla\CMS\Factory;
-use Joomla\CMS\Form\Form;
+use Joomla\CMS\Form\FormFactoryInterface;
+use Joomla\CMS\MVC\Factory\MVCFactoryInterface;
 use Joomla\CMS\MVC\Model\AdminModel;
 
 class ArticleModel extends AdminModel
 {
+	public function __construct($config = [], MVCFactoryInterface $factory = null, FormFactoryInterface $formFactory = null)
+	{
+		/**
+		 * Well, Joomla 4 core developers seem to be writing buggy a.f. code. The onContentChangeState handler of a core
+		 * plugin ASSUMES that if the context contains the word "article" it must be com_content (an idiotic assumption,
+		 * since the context contains, dunno, THE COMPONENT NAME?!!!) and generates invalid SQL for our extension. It
+		 * looks like something related to workflows but I won't go into debugging THAT.
+		 *
+		 * Therefore I need to tell the AdminModel code to NOT use onContentChangeState. I can't. I have to tell it to
+		 * use something different. So I am using something which describes precisely my problem.
+		 */
+		$config['event_change_state'] = 'onPleaseStopBreakingMyCodeWithBadlyWrittenCorePluginsForCryingOutLoud';
+
+		parent::__construct($config, $factory, $formFactory);
+	}
+
+
 	public function getForm($data = [], $loadData = true)
 	{
 		return $this->loadForm(
